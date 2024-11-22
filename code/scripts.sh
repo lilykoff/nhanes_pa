77ee96a00405ee3c581eca6690cb3331a26a55ee
lilykoff <koffmanlily@gmail.com>
2024-11-22 06:31:26 -0500

    agecat add

--- code/scripts.sh
+++ code/scripts.sh
@@ -1,1 +1,1 @@
-Rnosave 04_fui.R -J FUI --mem=150G --time=7-00 -o eofiles/%x_%A.out -e eofiles/%x_%A.err
+Rnosave 04_fui.R -J FUI --mem=150G --time=7-00 --nodelist=compute-102 -o eofiles/%x_%A.out -e eofiles/%x_%A.err
